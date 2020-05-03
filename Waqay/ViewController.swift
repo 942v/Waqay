1451e30dd9768514a21a7ce624413984f3fd4fc0
Guillermo Sáenz <gsaenz@proatomicdev.com>
2020-05-03 17:16:19 -0500

    Initial Commit

---
+++ Waqay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Waqay
+//
+//  Created by Guillermo Sáenz on 5/3/20.
+//  Copyright © 2020 Property Atomic Strong SAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WaqayTests/WaqayTests.swift
@@ -0,0 +1,34 @@
+//
+//  WaqayTests.swift
+//  WaqayTests
+//
+//  Created by Guillermo Sáenz on 5/3/20.
+//  Copyright © 2020 Property Atomic Strong SAC. All rights reserved.
+//
+
+import XCTest
+@testable import Waqay
+
+class WaqayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
